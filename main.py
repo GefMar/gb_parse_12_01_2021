09e273fbe8e63d5131d8d51b942f1c2e9f47e043
gefest <gefest@me.com>
2021-01-27 19:53:30 +0300

    les 5

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
-from gb_parse.spiders.autoyuola import AutoyuolaSpider
+from gb_parse.spiders.autoyuola import AutoyoulaSpider
 
 if __name__ == "__main__":
     crawler_settings = Settings()
     crawler_settings.setmodule("gb_parse.settings")
     crawler_process = CrawlerProcess(settings=crawler_settings)
-    crawler_process.crawl(AutoyuolaSpider)
+    crawler_process.crawl(AutoyoulaSpider)
     crawler_process.start()
